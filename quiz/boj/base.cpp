4c2df175b4d9c3fb9a7e8e5182d1891848147c26
Jeff Chung <drifterz303@gmail.com>
2017-09-26 18:48:53 +0900

    백준 답안 제출시 기본 입력폼

---
+++ quiz/boj/base.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+void solution(){
+    
+}
+
+int main(){
+    
+    
+    return 0;
+    
+}
