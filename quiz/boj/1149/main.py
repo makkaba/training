65d9f3510f4e1c17deeae4092a94f97eb2a2c257
Jeff Chung <drifterz303@gmail.com>
2017-11-12 14:16:49 +0900

    1149 문제 솔루션

--- quiz/boj/1149/main.py
+++ quiz/boj/1149/main.py
@@ -1,26 +1,21 @@
-
-
-n = input()
+n = int(input())
 arr = []
 for i in range(n):
-    arr[i].append([int(v) for v in input().split()])
+    arr.append([int(v) for v in input().split()])
 
 '''
-D[i][j] = i번째에 j를 놨을 때 최소 비용
-D[i][0] = max (D[i-1][1], D[i-1][2])
-D[i][1] = max (D[i-1][0], D[i-1][2])
-D[i][2] = max (D[i-1][0], D[i-1][1])
-
-return max(D[n][0],D[n][1],D[n][2])
-
+D[i][j] = i 번째에 j를 놨을 때 누적 최소값
+D[i][j] = min(D[i-i][j]+)
 '''
+
 
 D = [[0 for i in range(3)] for j in range(n)]
 D[0][0] = arr[0][0]
 D[0][1] = arr[0][1]
 D[0][2] = arr[0][2]
 
-for i in range(1, n+1):
+for i in range(1, n):
     for j in range(3):
-        D[i][j] = D[i][j]
+        D[i][j] = min(D[i-1][(j+1)%3], D[i-1][(j+2)%3]) + arr[i][j]
 
+print(min(D[n-1][0], D[n-1][1], D[n-1][2]))
